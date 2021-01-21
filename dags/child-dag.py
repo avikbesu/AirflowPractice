950217db84e7ca2c7f071b76597e02bdbcd29e0b
avik.burdwan@gmail.com <avik.burdwan@gmail.com>
2021-01-21 23:39:39 +0530

    triggering dags

--- dags/child-dag.py
+++ dags/child-dag.py
@@ -1,36 +1,36 @@
-# from datetime import datetime, timedelta
-# from airflow import DAG
-# from airflow.operators.dummy_operator import DummyOperator
-# from airflow.operators.sensors import ExternalTaskSensor
-# from airflow.utils import dates
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.sensors import ExternalTaskSensor
+from airflow.utils import dates
 
-# default_args = {
-#     "owner": "airflow",
-#     "depends_on_past": False,
-#     "start_date": dates.days_ago(1),
-# }
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": dates.days_ago(2),
+}
 
-# dag = DAG('Child_dag', default_args=default_args, schedule_interval=timedelta(minutes=5))
+dag = DAG('Child_dag', default_args=default_args, schedule_interval=timedelta(days=1))
 
-# # Use ExternalTaskSensor to listen to the Parent_dag and cook_dinner task
-# # when cook_dinner is finished, Child_dag will be triggered
-# wait_for_dinner = ExternalTaskSensor(
-#     task_id='wait_for_dinner',
-#     external_dag_id='Parent_dag',
-#     external_task_id='cook_dinner',
-#     start_date=dates.days_ago(1),
-#     execution_delta=timedelta(minutes=1),
-#     timeout=60,
-# )
+# Use ExternalTaskSensor to listen to the Parent_dag and cook_dinner task
+# when cook_dinner is finished, Child_dag will be triggered
+wait_for_dinner = ExternalTaskSensor(
+    task_id='wait_for_dinner',
+    external_dag_id='Parent_dag',
+    external_task_id='leave_work',
+    start_date=dates.days_ago(2),
+    execution_delta=timedelta(days=1),
+    timeout=60,
+)
 
-# have_dinner = DummyOperator(
-#     task_id='have_dinner',
-#     dag=dag,
-# )
-# play_with_food = DummyOperator(
-#     task_id='play_with_food',
-#     dag=dag,
-# )
+have_dinner = DummyOperator(
+    task_id='have_dinner',
+    dag=dag,
+)
+play_with_food = DummyOperator(
+    task_id='play_with_food',
+    dag=dag,
+)
 
-# wait_for_dinner >> have_dinner
-# wait_for_dinner >> play_with_food
+wait_for_dinner >> have_dinner
+wait_for_dinner >> play_with_food
--- dags/parent-dag.py
+++ dags/parent-dag.py
@@ -1,23 +1,26 @@
-# from datetime import datetime, timedelta
-# from airflow import DAG
-# from airflow.operators.dummy_operator import DummyOperator
-# from airflow.utils import dates
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils import dates
+from airflow.operators import MyFirstOperator
 
-# default_args = {
-#     "owner": "airflow",
-#     "depends_on_past": False,
-#     "start_date": dates.days_ago(1),
-# }
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": dates.days_ago(2),
+}
 
-# dag = DAG('Parent_dag', default_args=default_args, schedule_interval=timedelta(minutes=5))
+dag = DAG('Parent_dag', default_args=default_args, schedule_interval=timedelta(days=1))
 
-# leave_work = DummyOperator(
-#     task_id='leave_work',
-#     dag=dag,
-# )
-# cook_dinner = DummyOperator(
-#     task_id='cook_dinner',
-#     dag=dag,
-# )
+leave_work = MyFirstOperator(
+	my_operator_param='leave_work...',
+    task_id='leave_work', 
+    dag=dag,
+)
+cook_dinner = MyFirstOperator(
+	my_operator_param='cook_dinner!!!',
+    task_id='cook_dinner',
+    dag=dag,
+)
 
-# leave_work >> cook_dinner
+leave_work >> cook_dinner
